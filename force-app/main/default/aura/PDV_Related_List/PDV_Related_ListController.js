aad7d9967642b635ef08dd2735e73abc0b248cd5
kirankpACN <kiran.a.kumar.p@accenture.com>
2020-10-16 11:19:09 +1100

    pdv components

---
+++ force-app/main/default/aura/PDV_Related_List/PDV_Related_ListController.js
@@ -0,0 +1,9 @@
+({
+    doInit: function(component, event, helper){
+        helper.setRelatedId(component, event, helper);
+	},
+    
+    recordUpdated: function(component, event, helper) {
+        helper.setRelatedId(component, event, helper);
+    }
+})
